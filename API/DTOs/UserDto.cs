7f3267bf79f132b37882ae56f7f62ffb618a6201
enaiamo <zelenckina1999@gmail.com>
2021-08-02 18:16:22 +0300

    field correction

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -3,6 +3,6 @@ namespace API.DTOs
     public class UserDto
     {
         public string UserName { get; set; }
-        public string UserName { get; set; }
+        public string Token { get; set; }
     }
 }
